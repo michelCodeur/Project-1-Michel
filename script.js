ad2f8f49701d5112c7fbe6747b32da8d1e33026c
michelCodeur <michelpathinvo@gmail.com>
2021-12-08 16:09:48 -0600

    index.html and style.css done

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 //Create constants
+//const obj = JSON.parse(capitalsList);
+let currentFlashCard = document.querySelector('#flashcards');
+const currentQuestion = 0;
+const arrQuestions = new Array();
 
 //cached data references
+function getList() {
+	for (i = 0; i < data.length; i++) {
+		arrQuestions[i] = [];
+		arrQuestions[i][0] = data[i].front;
+		arrQuestions[i][1] = data[i].list1;
+		arrQuestions[i][2] = data[i].list2;
+		arrQuestions[i][3] = data[i].list3;
+		arrQuestions[i][4] = data[i].back;
+	}
+}
+getList();
+
+function startGame(getList, )
+
+    currentFlashCard = 0,
+    currentFlashCard.append(arrQuestions[i][0]);
+    currentFlashCard.append(arrQuestions[i][1]);
+    currentFlashCard.append(arrQuestions[i][2]);
+    currentFlashCard.append(arrQuestions[i][3]);
+    currentFlashCard.addEventListener('click', function() {
+    currentQuestion++;
+    
+})
+
+
+startGame();
+
+//function start
 
 //event listeners
 
