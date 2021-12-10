3f8c366d96e6bb161787d4b124ea2f2abcca7f21
michelCodeur <michelpathinvo@gmail.com>
2021-12-09 20:37:56 -0600

    script.js almost done

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
 //Create constants
-//const obj = JSON.parse(capitalsList);
-let currentFlashCard = document.querySelector('#flashcards');
-const currentQuestion = 0;
+const previousBtn = document.querySelector('#previous');
+previousBtn.addEventListener('click', moveToPreviousPage);
+const nextBtn = document.querySelector('#next');
+nextBtn.addEventListener('click', moveToNextPage);
+const panel = document.querySelector('#panel');
+//panel.addEventListener('click', startGame);
+const clickHereForRandQuestionBtn = document.querySelector('#randomQuestion');
+const question = document.querySelector('#question');
+const choice1 = document.querySelector('#choice1');
+const choice2 = document.querySelector('#choice2');
+const choice3 = document.querySelector('#choice3');
+const answer = document.querySelector('#answer');
+let currentQuestion = 0;
+let currentAnswer = 0;
+let isAnswerShown = false;
 const arrQuestions = new Array();
 
-//cached data references
-function getList() {
-	for (i = 0; i < data.length; i++) {
-		arrQuestions[i] = [];
-		arrQuestions[i][0] = data[i].front;
-		arrQuestions[i][1] = data[i].list1;
-		arrQuestions[i][2] = data[i].list2;
-		arrQuestions[i][3] = data[i].list3;
-		arrQuestions[i][4] = data[i].back;
-	}
+const randomNum = Math.floor(Math.random() * data.length);
+
+function renderQuestion() {
+	answer.classList.toggle('hidden');
+	//produce a random flashcard question from arrQuestions
+	const randomNum = Math.floor(Math.random() * data.length);
+	//clickHereForRandQuestionBtn.addEventListener('click', randomQuestion)
+	question.innerText = data[randomNum].question;
+	choice1.innerText = data[randomNum].choice1;
+	choice2.innerText = data[randomNum].choice2;
+	choice3.innerText = data[randomNum].choice3;
+	answer.innerText = data[randomNum].answer;
+	// currentQuestion++;
+	//reveal the answer on the back of the flashcard
+	clickHereForRandQuestionBtn.innerText = 'CLICK HERE TO REVEAL ANSWER';
 }
-getList();
+clickHereForRandQuestionBtn.addEventListener('click', function () {
+	if (isAnswerShown) {
+		renderQuestion();
+	} else {
+		revealAnswer();
+	}
+	isAnswerShown = !isAnswerShown;
+});
 
-function startGame(getList, )
+renderQuestion();
 
-    currentFlashCard = 0,
-    currentFlashCard.append(arrQuestions[i][0]);
-    currentFlashCard.append(arrQuestions[i][1]);
-    currentFlashCard.append(arrQuestions[i][2]);
-    currentFlashCard.append(arrQuestions[i][3]);
-    currentFlashCard.addEventListener('click', function() {
-    currentQuestion++;
-    
-})
+function revealAnswer() {
+	answer.classList.toggle('hidden');
 
+	clickHereForRandQuestionBtn.innerText = 'CLICK HERE FOR RANDOM QUESTION';
+	//const randomNum = Math.floor(Math.random() * data.length);
+}
 
-startGame();
+// currentAnswer = panel.addEventListener('click', revealAnswer);
+
+function moveToPreviousPage() {
+	If;
+}
+function moveToNextPage() {}
 
 //function start
 
